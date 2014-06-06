b7a1670bc881d80985aee7a42c1fa4e79ec5c4a2
johnymonster <jnelson11@gmail.com>
2014-06-06 09:25:15 -0400

    Winston and expressjs were far to advanced for the needs of a simple messaging service.

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
-var express     = require('express')
-  , bodyParser  = require('body-parser')
-  , winston     = require('winston');
+var http = require('http')
+  , util = require('util');
 
-var logger = new (winston.Logger)({
-  transports: [
-    new (winston.transports.Console)({timestamp: true, colorize: true})
-  ]
+var server = http.createServer(function(req, res){
+  req.setEncoding('utf8');
+  req.on('data', function(data){
+    util.log(data);
+  });
+
+  req.on('end', function(){
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('OK');
+  });
+
+  req.on('error', function(err){
+    console.log(err);
+  });
 });
 
-var app = express();
-
-app.use(bodyParser());
-
-app.get('/', function(req, res){
-  logger.info('request params', req.params);
-  logger.info('request queryString',req.query);
-  res.send(200);
-});
-
-app.post('/', function(req, res){
-  logger.info(req.body);
-  res.send(200);
-})
-
-app.listen(3000);
+server.listen(3000);
