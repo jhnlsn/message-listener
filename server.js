21f7e3917444258249bb1df95483ed25c3a330a0
johnymonster <jnelson11@gmail.com>
2014-06-06 10:17:47 -0400

    Log the full message at once.

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ var http = require('http')
   , util = require('util');
 
 var server = http.createServer(function(req, res){
+  var message = '';
   req.setEncoding('utf8');
   req.on('data', function(data){
-    util.log(data);
+    message += data;
   });
 
   req.on('end', function(){
+    util.log(message);
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('OK');
   });
